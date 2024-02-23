be04f59ff0c1becd8c64af6887654d8baa12ac8d
JoelAngel <joelbaraka4325@gmail.com>
2024-02-23 10:44:16 +0300

    text container set

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,13 @@ export default function Home() {
   return (
     <main>
       <Hero />
+      {/* Car Catalogue */}
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+      </div>
     </main>
   );
 }
